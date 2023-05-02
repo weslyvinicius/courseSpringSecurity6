5652081b34afad757353b745a003e459318abe2c
Wesly <weslyvinicius@hotmail.com>
2023-05-02 19:32:25 +0100

    projeto inicial

---
+++ src/main/java/com/academy/springsecurity6full/SpringSecurity6FullApplication.java
@@ -0,0 +1,13 @@
+package com.academy.springsecurity6full;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6FullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6FullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/springsecurity6full/SpringSecurity6FullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.springsecurity6full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6FullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
