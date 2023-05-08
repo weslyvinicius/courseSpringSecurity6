9299bd79816fafbff2fb34b5b6b09376843e965e
Wesly <weslyvinicius@hotmail.com>
2023-05-08 18:09:10 +0100

    add forms default

--- src/main/java/com/academy/springsecurity6full/config/SecurityConfig.java
+++ src/main/java/com/academy/springsecurity6full/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.academy.springsecurity6full.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,11 +13,14 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain mySecurityFilterChain( HttpSecurity http ) throws Exception {
 
-		http.csrf().disable()
+		http.csrf()
+				.disable()
 				.authorizeHttpRequests()
 				.requestMatchers( HttpMethod.GET,"/myfree" ).permitAll()
+				.requestMatchers( "/logout" ).permitAll()
 				.anyRequest().authenticated()
 				.and()
+				.formLogin( Customizer.withDefaults())
 				.httpBasic()
 				.and()
 				.cors();
