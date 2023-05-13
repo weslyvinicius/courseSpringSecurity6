4c400a93b2c0e155190453f18b07674e05285669
Wesly <weslyvinicius@hotmail.com>
2023-05-13 01:44:09 +0100

    criando srestrição base authentication com roles na config

--- src/main/java/com/academy/springsecurity6full/config/SecurityConfig.java
+++ src/main/java/com/academy/springsecurity6full/config/SecurityConfig.java
@@ -4,12 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,17 +16,25 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain mySecurityFilterChain( HttpSecurity http ) throws Exception {
 
-		http.csrf()
-				.disable()
-				.authorizeHttpRequests()
-				.requestMatchers( HttpMethod.GET,"/myfree" ).permitAll()
-				.requestMatchers( "/logout" ).permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.formLogin( Customizer.withDefaults())
-				.httpBasic()
-				.and()
-				.cors();
+		http.authorizeHttpRequests(configure ->
+			configure
+					.requestMatchers( HttpMethod.GET, "/api/employees").hasRole( "EMPLOYEE" )
+			        .requestMatchers( HttpMethod.GET, "/api/employees/**").hasRole( "EMPLOYEE" )
+			        .requestMatchers( HttpMethod.POST, "/api/employees").hasRole( "MANAGER" )
+			        .requestMatchers( HttpMethod.PUT, "/api/employees").hasRole( "MANAGER" )
+			        .requestMatchers( HttpMethod.DELETE, "/api/employees/**").hasRole( "ADMIN" )
+					// allow do acess to lougout default
+					.requestMatchers( "/logout" ).permitAll()
+		);
+
+		// use http basic authentication
+		http.httpBasic();
+
+		// disable csrf
+		http.csrf().disable();
+
+		//Enable form to login
+		http.formLogin( Customizer.withDefaults());
 
 		// http.addFilterBefore( new myFilter, UsernamePasswordAuthenticationFilter.class  ) // informo ao sprint security um filter
 		// a ser executado antes.
---
+++ src/main/java/com/academy/springsecurity6full/controller/EmployeesController.java
@@ -0,0 +1,39 @@
+package com.academy.springsecurity6full.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeesController {
+
+
+	@GetMapping
+	public String getAllOfEmployees(){
+    	return "Read all employees";
+	}
+
+	@GetMapping("/{employeeId}")
+	public String getEmployee( @PathVariable String employeeId ){
+		return "Read Employee";
+	}
+
+	@PostMapping
+	public String saveEmployee(){
+		return "Create Employee";
+	}
+
+	@PutMapping
+	public String updateEmployee(){
+		return "Update Employee";
+	}
+
+	@DeleteMapping("/{employeeId}")
+	public String deleteEmployee( @PathVariable String employeeId ){
+		return "Delete Employee";
+	}
+
+
+
+
+
+}
